ff52590828b71679d147f1924368e6e3cd4f5a56
Flavio Diogo <Flavio1887@hotmail.com>
2021-11-10 16:33:55 -0300

    Home Page e resert de estilo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Theme from './theme';
+import Home from './pages/Home/index';
+import { ThemeProvider } from 'styled-components';
+import {Reset} from 'styled-reset';
+import reset from 'styled-reset';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+              <div>      
+                  <ThemeProvider theme={Theme}>
+                      <Reset />
+                      <Home />
+                  </ThemeProvider>  
+              </div>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const Theme = {
+  colors: {
+    primary: '#6200EE',
+    background: '#6200EE',
+    text: '#000000b30',
+    sucess: '#135E01'
+  }, fonts:{
+    regular: 'roboto , sans-serif'
+  }
+};
+export default Theme
