656e1b729f228d1b68071a452299ca26afa5adea
Flavio Diogo <Flavio1887@hotmail.com>
2021-11-10 16:53:54 -0300

    Inicio do conteudo Lateral

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,11 @@
 import styled  from "styled-components";
 
 
-const Container = styled.div `
-  //background-color: ${(props) => props.theme.colors.primary};
+const Container = styled.aside `
+  background-color: ${(props) => props.theme.colors.background};
+  width: 36vw;
+  height: 100vh;
+  overflow-y: auto;
 `;
 
 export default Container;
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 const Theme = {
   colors: {
-    primary: '#6200EE',
-    background: '#6200EE',
+    primary: '#6200ee',
+    background: '#00000014',
     text: '#000000b30',
     sucess: '#135E01'
   }, fonts:{
